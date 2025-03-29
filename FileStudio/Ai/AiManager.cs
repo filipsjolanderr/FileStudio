d0052354555fdfe16410ae0b8471a4a730b79ce7
Filip Sjölander <filip.sjolander@gmail.com>
2025-03-29 15:40:44 +0100

    fixing ai

---
+++ FileStudio/Ai/AiManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStudio.Ai
+{
+    class AiManager
+    {
+        private string _apiKey;
+
+        public AiManager()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("AI_API_KEY");
+        }
+    }
+}
