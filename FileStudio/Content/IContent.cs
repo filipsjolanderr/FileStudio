ae1857d9b22849a54e91af797e0d83cddfcab9e7
Filip Sjölander <filip.sjolander@gmail.com>
2025-04-25 15:06:30 +0200

    fixed

--- FileStudio/Content/IContent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using FileStudio.FileManagement;
-
-namespace FileStudio.Content;
-
-public interface IContent
-{
-    public List<string> GetContent(string path);
-}
